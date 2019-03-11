915cb6a74c8b5c87e9137572b187b057bb360e8f
Dantas <xp1998@live.com.pt>
2019-03-11 17:59:14 +0000

    verificações nos registos

--- src/main.pl
+++ src/main.pl
@@ -15,20 +15,30 @@ servico(4, exame4, hospitaldoPorto, porto).
 
 % consulta(Data, IdUt, IdServico, Custo)
 :- dynamic consulta/4.
-consulta(9/03/2019, 1, 1, 10.0).
+% date(9,maio,1998)
+consulta(20/10/1998, 1, 1, 10.0).
 
 
+naturais(X) :-X =:= round(X), X >= 1.
+% inteiro(X) :- X =:= round(X).
+
+% checkString(S,R) :- atom_codes(S,R).
 
 % Registar utentes, serviços e consultas
-registarUtente(IdUt, Nome, Idade, Cidade) :- 
-    assert(utente(IdUt, Nome, Idade, Cidade)).
+% Caso id seja igual, remove a ocorrência anterior
+% Apenas permite o registo de ids e idade em número natural
+registarUtente(IdUt, Nome, Idade, Cidade) :- removerUtente(IdUt), naturais(IdUt), 
+                                             naturais(Idade),assert(utente(IdUt, Nome, Idade, Cidade)).
+registarUtente(IdUt, Nome, Idade, Cidade) :- naturais(IdUt), naturais(Idade), assert(utente(IdUt, Nome, Idade, Cidade)).
 
-registarServico(IdServico, Descricao, Instituicao, Cidade) :- 
-    assert(servico(IdServico, Descricao, Instituicao, Cidade)).
+registarServico(IdServico, Descricao, Instituicao, Cidade) :- removerServico(IdServico), 
+                                                              assert(servico(IdServico, Descricao, Instituicao, Cidade)).
+registarServico(IdServico, Descricao, Instituicao, Cidade) :- assert(servico(IdServico, Descricao, Instituicao, Cidade)).
 
-registarConsulta(Data, IdUt, IdServico, Custo) :- 
-    assert(consulta(Data, IdUt, IdServico, Custo)).
-
+registarConsulta(Data, IdUt, IdServico, Custo) :- removerConsulta(IdUt, IdServico), naturais(IdUt), utente(IdUt,_,_,_), servico(IdServico,_,_,_), 
+                                                  assert(consulta(Data, IdUt, IdServico, Custo)).
+registarConsulta(Data, IdUt, IdServico, Custo) :- naturais(IdUt), naturais(IdServico), utente(IdUt,_,_,_), servico(IdServico,_,_,_),  
+                                                  assert(consulta(Data, IdUt, IdServico, Custo)).
 
 % Remover utentes, serviços e consultas
 removerUtente(IdUt) :- 
