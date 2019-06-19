c071ae457f946fda880c3caa068afac048b378c7
Felfit <josefernandes.fel@gmail.com>
2019-06-19 16:52:53 +0100

    Reorganização do documento.

--- SRCR-2.2/main.pl
+++ SRCR-2.2/main.pl
@@ -4,12 +4,6 @@
 :- set_prolog_flag( single_var_warnings,off ).
 :- set_prolog_flag( unknown,fail ).
 
-nao(T):- T,!,fail.
-nao(T).
-
-si(T,verdadeiro):- T.
-si(T,falso):- -T.
-si(T,desconhecido):- nao(T),nao(-T).
 
 bool(true).
 -bool(false).
@@ -82,6 +76,8 @@ interdito(interdito).
 
 %Caso de Estudo
 
+%% Representar Conhecimento Positivo e Negativo
+
 %Utente
 :- dynamic utente/4.
 %%  utente(IdUt,Nome,Idade,Morada)
@@ -91,26 +87,6 @@ interdito(interdito).
         nao(utente(IdUt,Nome,Idade,Morada)),
         nao(excecao(utente(IdUt,Nome,Idade,Morada))).
 
-
-%% Invariantes
-%%% Inserção de Conhecimento
-%% O valor do id tem de ser inteiro e nao pode ser nao nulo
-+utente(IdUt,Nome,Idade,Morada) :: (nao(nulo(IdUt)),integer(IdUt)).
-
-
-%% Conhecimento positivo não pode ser negativo. 
-+utente(IdUt,Nome,Idade,Morada)::nao(-utente(IdUt,Nome,Idade,Morada)).
-
-%%% Remoção de Conhecimento
--(utente(IdUt,Nome,Idade,Morada))::(nao(interdito(Nome)),nao(interdito(Idade)),nao(interdito(Morada))).
-
-%% Não se pode adicionar conhecimento negativo com nulos. 
-+(-utente(IdUt,Nome,Idade,Morada))::(nao(nulo(Nome))).
-%% A idade tem de ser um inteiro.
-+(-utente(IdUt,Nome,Idade,Morada))::(nao(nulo(Idade)),integer(Idade)).
-+(-utente(IdUt,Nome,Idade,Morada))::(nao(nulo(Morada))).
-
-%% Exemplos
 %% Exemplos de Conhecimento Positivo
     %% O utente jorge tem 25 anos e mora em braga
 utente(1,jorge,25,braga).
@@ -118,7 +94,27 @@ utente(1,jorge,25,braga).
 %% Exemplos de Conhecimento Negativo
     %% Sabe-se que a utente numero 2, Maria tem mais do que 40 anos
         %% Logo a Maria não pode ter 40 ou menos anos
--utente(2,maria,IDADE,_):- IDADE =< 40.
+-utente(2,maria,IDADE,Morada):- IDADE =< 40.
+
+%Prestador
+    %% prestador(idPrest,Nome,Especialidade,Instituicao)
+
+:- dynamic prestador/4.
+
+%% Conhecimento Negativo
+-prestador(IdPrest,Nome,Especialidade,Instituicao):-
+        nao(prestador(IdPrest,Nome,Especialidade,Instituicao)),
+        nao(excecao(prestador(IdPrest,Nome,Especialidade,Instituicao))).
+
+%Cuidado
+    %% cuidado(Data,IdUt,IdPrest,Descricao,Custo)
+:- dynamic cuidado/5.
+
+%% Descrição não pode ser nula. Pode ser vazia.
+
+%%--------------------------------------------------------------------------------------------------------
+
+%% Representar casos de conhecimento Imperfeito, pela utilização de valores nulos de todos os tipos estudados;
 
 %% Exemplo de conhecimento Incerto
     %% O utente 2 chama-se Maria e mora em guimaraes. A sua idade é incerta, uma vez que não se pergunta a idade a uma senhora
@@ -155,7 +151,7 @@ excecao(utente(5,joaquim,_,guimaraes)).
 %% Mistura de conhecimento Impreciso+Interdito
     %% Sabe-se que a idade do utente 6 Gustavo está entre o intervalo [30,40[
     %% Por motivos alheios a sua morada está interdita
-excecao(utente(6,gustavo,Idade,interdito)):- Idade<40, Idade>=30.
+excecao(utente(6,gustavo,Idade,_)):- Idade<40, Idade>=30.
 +utente(IdUt,Nome,Idade,Morada) :: (
                     findall(
                         (IdUt,Nome,Idade,MS),
@@ -163,27 +159,67 @@ excecao(utente(6,gustavo,Idade,interdito)):- Idade<40, Idade>=30.
                         S ),
                     comprimento( S,N ), N == 0 
                   ).
++(-utente(IdUt,Nome,Idade,Morada)) :: (
+                    findall(
+                        (IdUt,Nome,Idade,MS),
+                        (-utente(6,gustavo,Idade,MS), nao(interdito(MS))),
+                        S ),
+                    comprimento( S,N ), N == 0 
+                  ).
 
-%% Exemplo de predicado que adiciona várias exceções
-addUtentesImprecisos(Id,impreciso([X|T]),I,M):- addUtentesImprecisos(Id,X,I,M),addUtentesImprecisos(Id,impreciso(T),I,M).
-addUtentesImprecisos(Id,impreciso([]),I,M).
-addUtentesImprecisos(Id,N,impreciso([X|T]),M):- addUtentesImprecisos(Id,N,X,M),addUtentesImprecisos(Id,N,impreciso(T),M).
-addUtentesImprecisos(Id,N,impreciso([]),M).
-addUtentesImprecisos(Id,N,I,impreciso([X|T])):- addUtentesImprecisos(Id,N,I,X),addUtentesImprecisos(Id,N,I,impreciso(T)).
-addUtentesImprecisos(Id,N,I,impreciso([])).
-addUtentesImprecisos(Id,N,I,M):- evolucaoExcecao(utente(Id,N,I,M)).
+%%--------------------------------------------------------------------------------------------------------
+
+%% Manipular invariantes que designem restrições à inserção e à remoção de conhecimento do sistema;
+
+%% Invariantes
+%%% Inserção de Conhecimento
+%% O valor do id tem de ser inteiro e nao pode ser nao nulo
++utente(IdUt,Nome,Idade,Morada) :: (nao(nulo(IdUt)),integer(IdUt)).
 
 
-%Prestador
-    %% prestador(idPrest,Nome,Especialidade,Instituicao)
+%% Conhecimento positivo não pode ser negativo. 
++utente(IdUt,Nome,Idade,Morada)::nao(-utente(IdUt,Nome,Idade,Morada)).
 
-:- dynamic prestador/4.
+%%% Remoção de Conhecimento
+-(utente(IdUt,Nome,Idade,Morada))::(nao(interdito(Nome)),nao(interdito(Idade)),nao(interdito(Morada))).
 
-%% Conhecimento Negativo
--prestador(IdPrest,Nome,Especialidade,Instituicao):-
-        nao(prestador(IdPrest,Nome,Especialidade,Instituicao)),
-        nao(excecao(prestador(IdPrest,Nome,Especialidade,Instituicao))).
+%% Não se pode adicionar conhecimento negativo com nulos. 
++(-utente(IdUt,Nome,Idade,Morada))::(nao(nulo(Nome))).
+%% A idade tem de ser um inteiro.
++(-utente(IdUt,Nome,Idade,Morada))::(nao(nulo(Idade)),integer(Idade)).
++(-utente(IdUt,Nome,Idade,Morada))::(nao(nulo(Morada))).
 
+%% Invariante
+    %% Por motivos legais não podem ser atendidos utentes das ilhas da madeira e dos açores
+ilhas(madeira).
+ilhas(acores).
++utente(IdUt,Nome,Idade,Morada)::(nao(ilhas(Morada))).
+
+%%TODO Exemplos
+%% Exemplos de Conhecimento Positivo
+    %% O prestador juan especializa-se em ortopedia no hospital de braga
+prestador(1,juan,ortopedia,hospitalbraga).
+
+%% Exemplos de Conhecimento Negativo
+    %% Sabe-se que o carlos não trabalha no hospital do porto
+-prestador(2,carlos,_,hospitalporto).
+
+%% Exemplo de conhecimento Incerto
+    %% O prestador 2 carlos presta consultas de nutrição, não se sabe no entanto em que local
+prestador(2,carlos,nutricao,incerto).
+excecao(prestador(IdUt,Nome,Especialidade,Local)):- prestador(IdUt,Nome,Especialidade,incerto).
+
+%% Exemplo de conhecimento Interdito
+    %% Por motivos judiciais é impossivel aceder ao nome do prestador 3
+        %% Este prestador especializa-se em cirurgia geral no hospital da beira
+prestador(3,interdito,cirurgiageral,hospitalbeira).
+
+%% Exemplo de conhecimento Impreciso
+    %% Houve uma corrupção no sistema de dados.
+    %% Por causa disto é impossivel determinar se o prestador antonio se especializa em Oncologia ou Oftalmologia.
+    %% Sabe-se que o servico é prestado em lisboa. 
+excecao(prestador(4,antonio,oncologia,hospitallisboa)).
+excecao(prestador(4,antonio,oftalmologia,hospitallisboa)).
 
 %% Invariantes
 %%% Inserção de Conhecimento
@@ -226,40 +262,6 @@ addUtentesImprecisos(Id,N,I,M):- evolucaoExcecao(utente(Id,N,I,M)).
 +(-prestador(IdPrest,Nome,Especialidade,Instituicao))::nao(nulo(Instituicao)).
 
 
-
-%%TODO Exemplos
-%% Exemplos de Conhecimento Positivo
-    %% O prestador juan especializa-se em ortopedia no hospital de braga
-prestador(1,juan,ortopedia,hospitalbraga).
-
-%% Exemplos de Conhecimento Negativo
-    %% Sabe-se que o carlos não trabalha no hospital do porto
--prestador(2,carlos,_,hospitalporto).
-
-%% Exemplo de conhecimento Incerto
-    %% O prestador 2 carlos presta consultas de nutrição, não se sabe no entanto em que local
-prestador(2,carlos,nutricao,incerto).
-excecao(prestador(IdUt,Nome,Especialidade,Local)):- prestador(IdUt,Nome,Especialidade,incerto).
-
-%% Exemplo de conhecimento Interdito
-    %% Por motivos judiciais é impossivel aceder ao nome do prestador 3
-        %% Este prestador especializa-se em cirurgia geral no hospital da beira
-prestador(3,interdito,cirurgiageral,hospitalbeira).
-
-%% Exemplo de conhecimento Impreciso
-    %% Houve uma corrupção no sistema de dados.
-    %% Por causa disto é impossivel determinar se o prestador antonio se especializa em Oncologia ou Oftalmologia.
-    %% Sabe-se que o servico é prestado em lisboa. 
-excecao(prestador(4,antonio,oncologia,hospitallisboa)).
-excecao(prestador(4,antonio,oftalmologia,hospitallisboa)).
-
-%Cuidado
-    %% cuidado(Data,IdUt,IdPrest,Descricao,Custo)
-:- dynamic cuidado/5.
-
-%% Descrição não pode ser nula. Pode ser vazia.
-
-
 %Invariante
 +cuidado(Data,IdUt,IdPrest,Descricao,Custo)::
         (nulo(NUL1), nulo(NUL2), nulo(NUL3), nulo(NUL4), nao( cuidado(NUL1,NUL2,NUL3,Descricao,NUL4))).
@@ -286,14 +288,33 @@ excecao(prestador(4,antonio,oftalmologia,hospitallisboa)).
 +(-cuidado(Data,IdUt,IdPrest,Descricao,Custo))::nao(nulo(Descricao)).
 +(-cuidado(Data,IdUt,IdPrest,Descricao,Custo))::nao(nulo(Custo)).
 
-        
-%% Exemplos
-%% Exemplos de Conhecimento Positivo
+%%--------------------------------------------------------------------------------------------------------
 
-%% Exemplos de Conhecimento Negativo
+%% Lidar com a problemática da evolução do conhecimento, criando os procedimentos adequados;
 
-%% Exemplo de conhecimento Incerto
+%% Exemplo de predicado que adiciona várias exceções
+addUtentesImprecisos(Id,impreciso([X|T]),I,M):- addUtentesImprecisos(Id,X,I,M),addUtentesImprecisos(Id,impreciso(T),I,M).
+addUtentesImprecisos(Id,impreciso([]),I,M).
+addUtentesImprecisos(Id,N,impreciso([X|T]),M):- addUtentesImprecisos(Id,N,X,M),addUtentesImprecisos(Id,N,impreciso(T),M).
+addUtentesImprecisos(Id,N,impreciso([]),M).
+addUtentesImprecisos(Id,N,I,impreciso([X|T])):- addUtentesImprecisos(Id,N,I,X),addUtentesImprecisos(Id,N,I,impreciso(T)).
+addUtentesImprecisos(Id,N,I,impreciso([])).
+addUtentesImprecisos(Id,N,I,M):- evolucaoExcecao(utente(Id,N,I,M)).
 
-%% Exemplo de conhecimento Interdito
+%%--------------------------------------------------------------------------------------------------------
 
-%% Exemplo de conhecimento Impreciso
+%% Desenvolver um sistema de inferência capaz de implementar os mecanismos de raciocínio inerentes a estes sistemas
+
+nao(T):- T,!,fail.
+nao(T).
+
+si(T,verdadeiro):- T.
+si(T,falso):- -T.
+si(T,desconhecido):- nao(T),nao(-T).
+
+siList([X|T],verdadeiro):- siList(T,verdadeiro) ,X.
+siList([],verdadeiro).
+siList([X|T],falso):- siList(T,_),-X.
+siList([X|T],falso):- siList(T,falso),X.
+siList([X|T],falso):- siList(T,falso),-X.
+siList([X|T],desconhecido):- siList(T,_), nao(X), nao(-X).
